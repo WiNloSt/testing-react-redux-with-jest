cbdee6a947d0f5e24852a79316b945f85c00ef71
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-03-20 13:11:44 +0700

    add some tests

---
+++ src/components/__tests__/TodoItem.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoItem from '../TodoItem'
+
+describe('TodoItem', () => {
+  let component
+
+  beforeEach(() => {
+    component = shallow(<TodoItem text='Hello' />)
+  })
+
+  it('matches the snapshot - not complete', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('matches the snapshot - Complete', () => {
+    component.setProps({ complete: true })
+
+    expect(component).toMatchSnapshot()
+  })
+
+  it('renders correct structure', () => {
+
+    expect(component.is('li')).toBe(true)
+  })
+
+  it('has complete class if receiving complete prop', () => {
+    component.setProps({ complete: true })
+
+    expect(component.hasClass('complete')).toBe(true)
+    expect(component.text()).toBe('Hello')
+  })
+})
